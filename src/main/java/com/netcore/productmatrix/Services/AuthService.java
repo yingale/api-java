d5c65c10607e846e4ba38ef5ccb19197270f83b3
yogesh.ingale <yogesh.ingale@netcore.co.in>
2019-01-08 19:26:07 +0530

    Added changes for property and pom xml

--- src/main/java/com/netcore/productmatrix/Services/AuthService.java
+++ src/main/java/com/netcore/productmatrix/Services/AuthService.java
@@ -35,27 +35,8 @@ public class AuthService {
 
 	public Boolean verify(String username, String password) {
 		try {
-			Properties properties = new Properties();
-			properties.setProperty("mail.store.protocol", "imaps");
-			properties.put("mail.imaps.host", "sm1.netcore.in");
-			properties.put("mail.imaps.port", 993);
-			properties.put("mail.imaps.fetchsize", 10);
-			properties.put("mail.imaps.starttls.enable", "false");
-			properties.put("mail.imaps.ssl.checkserveridentity", "false");
-			properties.setProperty("mail.imap.connectiontimeout", "5000");
-			properties.setProperty("mail.imap.timeout", "5000");
-			MailSSLSocketFactory socketFactory= new MailSSLSocketFactory();
-			socketFactory.setTrustAllHosts(true);
-			properties.put("mail.imaps.ssl.socketFactory", socketFactory);
-			properties.setProperty("mail.imaps.ssl.trust", "*");
-			Session emailSession = Session.getInstance(properties);
-//			emailSession.setDebug(true);
-			Store store = emailSession.getStore("imaps");
-			store.connect(username, password);
-			store.close();
+			System.err.println("Need to write login code");
 			return true;
-		} catch(AuthenticationFailedException e) {
-			return false;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
